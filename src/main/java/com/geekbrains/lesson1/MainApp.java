1008e56dff7fd20b02389f15f672cfb9ba25fba8
DeN <den.denis.denis@mail.ru>
2022-12-04 20:12:10 +0300

    lesson1

--- src/main/java/com/geekbrains/lesson1/MainApp.java
+++ src/main/java/com/geekbrains/lesson1/MainApp.java
@@ -1,6 +1,7 @@
 package com.geekbrains.lesson1;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Scanner;
@@ -12,62 +13,65 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.geekbrains.lesson1");
-        Cart cart = context.getBean(Cart.class);
-        ProductRepository productRepository = context.getBean(ProductRepository.class);
-        System.out.println("Добро пожаловать в магазин!");
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.geekbrains.lesson1")) {
+            Cart cart = context.getBean(Cart.class);
+            ProductRepository productRepository = context.getBean(ProductRepository.class);
+            System.out.println("Добро пожаловать в магазин!");
 
-        Scanner sc = new Scanner(System.in);
-        while (!quit) {
-            menuInfo("mainMenu");
-            int input = sc.nextInt();
+            Scanner sc = new Scanner(System.in);
+            while (!quit) {
+                menuInfo("mainMenu");
+                int input = sc.nextInt();
 
-            if (1 == input) {
-                productRepository.showAllProducts();
-            }
-            if ((2 == input)) {
-                boolean quitCart = false;
-                menuInfo("cartMenu");
-                while (!quitCart) {
-                    input = sc.nextInt();
-                    switch (input) {
-                        case 1 -> {
-                            System.out.println("Введите ID товара для добавления в корзину.");
-                            input = sc.nextInt();
-                            cart.addProduct(input);
-                            menuInfo("cartMenu");
-                        }
-                        case 2 -> {
-                            System.out.println("Введите ID товара для удаления из корзины.");
-                            input = sc.nextInt();
-                            cart.deleteProduct(input);
-                            menuInfo("cartMenu");
-                        }
-                        case 3 -> {
-                            cart.showCart();
-                            menuInfo("cartMenu");
-                        }
-                        case 4 -> quitCart = true;
-                        default -> System.out.println("Введены неккоректные данные! Попробуйте еще раз!");
-                    }
+                if (1 == input) {
+                    productRepository.showAllProducts();
                 }
+                if ((2 == input)) {
+                    boolean quitCart = false;
+                    menuInfo("cartMenu");
+                    while (!quitCart) {
+                        input = sc.nextInt();
+                        switch (input) {
+                            case 1 -> {
+                                System.out.println("Введите ID товара для добавления в корзину.");
+                                input = sc.nextInt();
+                                cart.addProduct(input);
+                                menuInfo("cartMenu");
+                            }
+                            case 2 -> {
+                                System.out.println("Введите ID товара для удаления из корзины.");
+                                input = sc.nextInt();
+                                cart.deleteProduct(input);
+                                menuInfo("cartMenu");
+                            }
+                            case 3 -> {
+                                cart.showCart();
+                                menuInfo("cartMenu");
+                            }
+                            case 4 -> quitCart = true;
+                            default -> System.out.println("Введены неккоректные данные! Попробуйте еще раз!");
+                        }
+                    }
+
+                }
+                if (3 == input) {
+                    System.out.println("Введите ID товара для его поиска.");
+                    input = sc.nextInt();
+                    if (productRepository.getProductById(input) != null) {
+                        System.out.println("\nРезультат поиска:\n" + productRepository.getProductById(input) + "\n");
+                    } else System.out.println("\nРезультат поиска:\nТовар с данным ID не найден!\n");
+                    input = 0;
+
+                }
+                if (4 == input) {
+                    System.out.println("До скорого!");
+                    quit = true;
+                }
+                if (input > 4) System.out.println("Введены неккоректные данные! Попробуйте еще раз!");
 
             }
-            if (3 == input) {
-                System.out.println("Введите ID товара для его поиска.");
-                input = sc.nextInt();
-                if (productRepository.getProductById(input) != null) {
-                    System.out.println("\nРезультат поиска:\n" + productRepository.getProductById(input) + "\n");
-                } else System.out.println("\nРезультат поиска:\nТовар с данным ID не найден!\n");
-                input = 0;
-
-            }
-            if (4 == input) {
-                System.out.println("До скорого!");
-                quit = true;
-            }
-            if (input > 4) System.out.println("Введены неккоректные данные! Попробуйте еще раз!");
-
+        } catch (BeansException e) {
+            e.printStackTrace();
         }
     }
 
